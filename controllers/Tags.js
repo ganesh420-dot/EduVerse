015b3ff63dccef428da7a3441021bf09ce2d54a7
Ganesh Chandra Das <ganeshchandradas233@gmail.com>
2025-09-07 00:32:23 +0530

    add Course, Profile, Section, Subsection, and Tags controllers

---
+++ controllers/Tags.js
@@ -0,0 +1,48 @@
+const Tag = require("../models/tags");
+
+exports.createTag = async (req, res) => {
+  try {
+    const { name, description } = req.body;
+
+    if (!name || !description) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const tagDetails = await Tag.create({
+      name: name,
+      description: description,
+    });
+    console.log(tagDetails);
+
+    return res.status(200).json({
+      success: true,
+      message: "Tag Created Successfully",
+    });
+  } catch (error) {
+    return res.this.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//getAllTags
+
+exports.showAlltags = async (req, res) => {
+  try {
+    const allTags = await Tag.find({}, { name: true, description: true });
+    res.status(200).json({
+      success: true,
+      message: "All tags returned successfully",
+      allTags,
+    });
+  } catch (error) {
+    return res.this.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
